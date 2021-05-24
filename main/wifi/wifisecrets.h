635bf85db800bf923400783091327b85327dc24d
hortenbach <gina.hortenbach@hhi.fraunhofer.de>
2021-05-24 14:16:49 +0200

    init wifi

--- main/wifi/wifisecrets.h
+++
@@ -1,8 +0,0 @@
-/**
-* Enter your wifi credentials
-**/
-#ifndef WIFI_SECRETS
-  #define CONFIG_WIFI_SSID        "hellossid"
-  #define CONFIG_WIFI_PASSWORD    "hellopsw"
-  #define CONFIG_MAXIMUM_RETRY    5
-#endif
