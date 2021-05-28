68ecd6d8fefbdd8c732103aa3720af71e5c11c1d
hortenbach <gina.hortenbach@hhi.fraunhofer.de>
2021-05-28 22:15:37 +0200

    ping

--- main/project.h
+++ main/project.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 // extern const char* TAG = "MyModule";
-#define TAG "MyModule"
+#define TAG "MQTT ECC APP"
 
 extern const uint8_t ca_pem_start[]   asm("_binary_ca_pem_start");
 extern const uint8_t ca_pem_end[]   asm("_binary_ca_pem_end");
